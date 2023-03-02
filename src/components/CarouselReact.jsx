b3fc5cb1b966f828f0712ced2cb8834b4d3fffe7
Nour Everaert <nour.everaert.prof@gmail.com>
2023-03-02 10:45:08 +0100

    appel api, carousel

--- src/components/CarouselReact.jsx
+++ src/components/CarouselReact.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import useEmblaCarousel from "embla-carousel-react";
+import Autoplay from "embla-carousel-autoplay";
 
-const CarouselReact = () => {
-  return <></>;
+const CarouselReact = ({ movies }) => {
+  const [emblaRef] = useEmblaCarousel({ loop: false }, [Autoplay()]);
+
+  return (
+    <>
+      <div className="embla" ref={emblaRef}>
+        <div className="embla__container">
+          {movies.map((movie, index) => (
+            <div key={index} className="embla__slide">
+              <h2>{movie.original_title}</h2>
+              <img
+                src={"https://image.tmdb.org/t/p/w500" + movie.poster_path}
+                alt={"Poster of" + movie.original_title}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default CarouselReact;
---
+++ src/components/Spotlight.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Spotlight = () => {
+  return (
+    <div>
+      <h1>Liste des films</h1>
+    </div>
+  );
+};
+
+export default Spotlight;
