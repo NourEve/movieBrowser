bfaad99c12851753319bbb88c3857e50d44b32e0
Nour Everaert <nour.everaert.prof@gmail.com>
2023-03-02 16:40:56 +0100

    filtre des genres + cartes films

--- src/components/ChildrenGenre.jsx
+++ src/components/ChildrenGenre.jsx
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 
 const ChildrenGenre = () => {
+  const { genreId } = useParams();
+  const [filterGenre, setFilterGenre] = useState([]);
+
+  useEffect(() => {
+    axios({
+      method: "get",
+      url: `https://api.themoviedb.org/3/discover/movie?api_key=af0a6e78cf2f5300726297b7f6e4d22c&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=2&with_genres=${genreId}&with_watch_monetization_types=flatrate`,
+      responseType: "json",
+    }).then((res) => setFilterGenre(res.data.results));
+  }, [genreId]);
+
   return (
     <div>
-      <p>Test</p>
+      {filterGenre.map((movie, index) => (
+        <div key={index}>
+          <img
+            src={"https://image.tmdb.org/t/p/w500" + movie.poster_path}
+            alt={"Poster of" + movie.original_title}
+          />
+          <h3>{movie.original_title}</h3>
+        </div>
+      ))}
     </div>
   );
 };
