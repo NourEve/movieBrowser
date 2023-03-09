9d841d6876b1218119b299dc8c37be1d170b575e
Nour Everaert <nour.everaert.prof@gmail.com>
2023-03-09 14:48:58 +0100

    design homepage

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
       </h1>
       <img
         className="header__logo"
-        src="../public/assets/images/blue_square_1-5bdc75aaebeb75dc7ae79426ddd9be3b2be1e342510f8202baf6bffa71d7f5c4.svg"
+        src="../src/assets/blue_square_1-5bdc75aaebeb75dc7ae79426ddd9be3b2be1e342510f8202baf6bffa71d7f5c4.svg"
         alt="TheMovieDB"
       />
     </div>
