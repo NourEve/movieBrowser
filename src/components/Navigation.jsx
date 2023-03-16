3abc5d419952e3a73ece9233eee935be338ac29a
Nour Everaert <nour.everaert.prof@gmail.com>
2023-03-16 11:21:44 +0100

    responsive tablette

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -3,19 +3,33 @@ import IconBxSearch from "../assets/IconBxSearch";
 import IconHome from "../assets/IconHome";
 import IconProfile from "../assets/IconProfile";
 import { NavLink } from "react-router-dom";
+import MediaQuery from "react-responsive";
 
 const Navigation = () => {
   return (
     <nav className="navigation">
-      <NavLink to={"/"}>
-        <IconHome width="9vw" height="9vw" />
-      </NavLink>
-      <NavLink to={"/catalog"}>
-        <IconBxSearch width="9vw" height="9vw" />
-      </NavLink>
-      <NavLink to={"/profile"}>
-        <IconProfile width="9vw" height="9vw" />
-      </NavLink>
+      <MediaQuery maxWidth={480}>
+        <NavLink to={"/"}>
+          <IconHome width="9vw" height="9vw" />
+        </NavLink>
+        <NavLink to={"/catalog"}>
+          <IconBxSearch width="9vw" height="9vw" />
+        </NavLink>
+        <NavLink to={"/profile"}>
+          <IconProfile width="9vw" height="9vw" />
+        </NavLink>
+      </MediaQuery>
+      <MediaQuery minWidth={481} maxWidth={768}>
+        <NavLink to={"/"}>
+          <IconHome width="6vw" height="6vw" />
+        </NavLink>
+        <NavLink to={"/catalog"}>
+          <IconBxSearch width="6vw" height="6vw" />
+        </NavLink>
+        <NavLink to={"/profile"}>
+          <IconProfile width="6vw" height="6vw" />
+        </NavLink>
+      </MediaQuery>
     </nav>
   );
 };
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import IconSearchOutline from "../assets/IconSearchOutline";
+import MediaQuery from "react-responsive";
 
 const SearchBar = ({ inputRef, onClick }) => {
   return (
     <div className="searchBar">
       <button className="searchBar__button" onClick={onClick}>
         <Link className="searchBar__button__link" to={`search`}>
-          <IconSearchOutline width="6vw" height="6vw" />
+          <MediaQuery maxWidth={480}>
+            <IconSearchOutline width="6vw" height="6vw" />
+          </MediaQuery>
+          <MediaQuery minWidth={481} maxWidth={768}>
+            <IconSearchOutline width="4vw" height="4vw" />
+          </MediaQuery>
         </Link>
       </button>
       <input
